f2af1b7d75e7800c7c9f0cf2d92a812403e29128
Miguel Rumbo <86128109+MiguelRumbo@users.noreply.github.com>
2023-11-05 17:42:53 -0600

    Ajustes (Sin Terminar)

--- Laberinto de la Mente/Assets/Scripts/SeguirJugador.cs
+++ Laberinto de la Mente/Assets/Scripts/SeguirJugador.cs
@@ -1,45 +1,27 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SeguirJugador : MonoBehaviour
 {
     public Transform jugador;
-    public float velocidadMovimiento = 5.0f;
-    public float rangoDeteccion = 10f; // Alcance de detección configurable desde el Inspector
+    private NavMeshAgent navMeshAgent;
     private Animator anim;
-    private float alturaInicial;
 
     void Start()
     {
-        anim = GetComponent<Animator>(); // Obtén el Animator del enemigo
-        alturaInicial = transform.position.y; // Guarda la posición Y inicial
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        // Verifica si el jugador está dentro del rango de detección
-        float distanciaAlJugador = Vector3.Distance(transform.position, jugador.position);
-
-        if (distanciaAlJugador < rangoDeteccion)
+        if (jugador != null)
         {
-            // Calcula la dirección hacia el jugador sin movimiento en el eje Y
-            Vector3 direccion = (jugador.position - transform.position);
-            direccion.y = 0;
-            direccion.Normalize();
-
-            // Mueve el enemigo hacia el jugador sin cambiar su altura
-            transform.position = new Vector3(transform.position.x, alturaInicial, transform.position.z);
-            transform.Translate(direccion * velocidadMovimiento * Time.deltaTime);
+            // Establece la posición del objetivo del agente de navegación al jugador
+            navMeshAgent.SetDestination(jugador.position);
 
             // Establece el parámetro "Move" en true para cambiar a la animación de caminar
-            anim.SetBool("Move", true);
-
-            // Rota el enemigo para que mire al jugador
-            transform.LookAt(jugador);
-        }
-        else
-        {
-            // Establece el parámetro "Move" en false para cambiar a la animación idle
-            anim.SetBool("Move", false);
+            anim.SetBool("Move", navMeshAgent.remainingDistance > navMeshAgent.stoppingDistance);
         }
     }
 }
